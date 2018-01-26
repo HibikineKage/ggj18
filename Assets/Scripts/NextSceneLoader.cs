c934c669e90d6c2d191364fababc03cc38fa76a9
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2018-01-27 04:44:04 +0900

    必要シーンの追加と仮組，それに伴うシーン遷移のスクリプト追加

---
+++ Assets/Scripts/NextSceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ggj2018
+{
+    public class NextSceneLoader : MonoBehaviour
+    {
+        public void LoadNextScene()
+        {
+            int loadingSceneIndex = SceneUtility.GetBuildIndexByScenePath(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(loadingSceneIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/ScenePathLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePathLoader : MonoBehaviour {
+
+    public string loadSceneName;
+
+	public void LoadScene()
+    {
+        SceneManager.LoadScene(loadSceneName);
+    }
+}
