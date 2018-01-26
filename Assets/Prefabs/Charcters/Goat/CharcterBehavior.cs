8882bcb757bc912d66b263590bdddb669a0e4605
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2018-01-27 03:33:38 +0900

    自機のスクリプトにnamespaceを追加　移動部分の間違いを修正

--- Assets/Prefabs/Charcters/Goat/CharcterBehavior.cs
+++ Assets/Prefabs/Charcters/Goat/CharcterBehavior.cs
@@ -2,43 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharcterBehavior : MonoBehaviour {
-
-    public float velocity;
-    public float jumpForce;
-    public int maxJumpFrequency;
-    public float groundCheckRayDirection;
-
-    Rigidbody rb;
-    int jumpFrequency;
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
-	
-	void Update () {
-        Walk();
-        Jump();
-	}
-
-    void Walk()
+namespace ggj2018
+{
+    public class CharcterBehavior : MonoBehaviour
     {
-        var vertical = Input.GetAxisRaw("Vertical");
-        var horizontal = Input.GetAxisRaw("Horizontal");
-        var deltaVelocity = new Vector3(horizontal,0, vertical).normalized*velocity+new Vector3(0,rb.velocity.y,0);
-        
-        rb.velocity = deltaVelocity;
-    }
 
-    void Jump()
-    {
-        if (Input.GetButtonDown("Fire1") && jumpFrequency < maxJumpFrequency)
+        public float velocity;
+        public float jumpForce;
+        public int maxJumpFrequency;
+        public float groundCheckRayDirection;
+
+        Rigidbody rb;
+        int jumpFrequency;
+        void Start()
         {
-            jumpFrequency++;
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            rb = GetComponent<Rigidbody>();
         }
 
-        var result = Physics.Raycast(transform.position, -transform.up, groundCheckRayDirection);
-        Debug.DrawLine(transform.position, transform.position - transform.up * groundCheckRayDirection);
-        if (result) jumpFrequency = 0;
+        void Update()
+        {
+            Walk();
+            Jump();
+        }
+
+        void Walk()
+        {
+            var vertical = Input.GetAxisRaw("Vertical");
+            var horizontal = Input.GetAxisRaw("Horizontal");
+            var deltaVelocity=(transform.forward* vertical + transform.right*horizontal).normalized*velocity + new Vector3(0, rb.velocity.y, 0);
+
+            rb.velocity = deltaVelocity;
+        }
+
+        void Jump()
+        {
+            if (Input.GetButtonDown("Fire1") && jumpFrequency < maxJumpFrequency)
+            {
+                jumpFrequency++;
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+
+            var result = Physics.Raycast(transform.position, -transform.up, groundCheckRayDirection);
+            Debug.DrawLine(transform.position, transform.position - transform.up * groundCheckRayDirection);
+            if (result) jumpFrequency = 0;
+        }
     }
 }
