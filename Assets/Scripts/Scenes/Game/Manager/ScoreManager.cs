1e197f3dae2179c821519e56266701c83c88e2aa
tomoki.umezawa <tomoki_umezawa_narumi@yahoo.co.jp>
2018-01-27 12:22:51 +0900

    Implementation ScoreManager

--- Assets/Scripts/Scenes/Game/Manager/ScoreManager.cs
+++ Assets/Scripts/Scenes/Game/Manager/ScoreManager.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ggj2018
 {
     public class ScoreManager : MonoSingleton<ScoreManager>
     {
-        public void AddScore(int playerNum, int addValue)
+        private int[] _badScores = new int[GameConstants.PlayerNum];
+            
+        public int GetBadScore(int playerNum)
         {
+            return _badScores[playerNum];
+        }
+
+        public void AddBadScore(int playerNum, int addValue)
+        {
+            _badScores[playerNum] =  _badScores[playerNum] + addValue;
         }
     }
 }
