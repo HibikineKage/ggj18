87f89bf4ed738c26f63a47b0c37e2c4ed0352ce8
tomoki.umezawa <tomoki_umezawa_narumi@yahoo.co.jp>
2018-01-27 12:15:46 +0900

    Add GameConstants

---
+++ Assets/Scripts/Scenes/Game/GameConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ggj2018
+{
+    public static class GameConstants
+    {
+        public const int PlayerNum = 4;
+        public const int StageNum = 4;
+        public const int GameLimitSec = 180;
+    }
+}
+
