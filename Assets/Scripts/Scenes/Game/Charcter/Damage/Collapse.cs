e5113b447e6861faef7665ff4f25e69416eab985
Hibikine Kage <goodsmile314159@gmail.com>
2018-01-27 16:17:20 +0900

    ダメージの処理

---
+++ Assets/Scripts/Scenes/Game/Charcter/Damage/Collapse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ggj2018
+{
+    public class Collapse : IDamage
+    {
+        const float collapseTime = 5.0f;
+        public float Damage()
+        {
+            return 5.0f;
+        }
+        public void OnDamaged(CharcterBehavior charcterBehavior)
+        {
+                charcterBehavior.StunTimer = collapseTime;
+                charcterBehavior.SetAnimationTrigger("Collapse");
+            }
+        }
+    }
+}
